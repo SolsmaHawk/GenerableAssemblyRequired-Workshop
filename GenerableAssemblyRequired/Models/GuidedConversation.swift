05515b0dcf003a4fe6bc26f2e3c6b89f4e7151dc
John Solsma <solsma@me.com>
2025-08-25 19:59:18 -0500

    Update comments

--- GenerableAssemblyRequired/Models/GuidedConversation.swift
+++ GenerableAssemblyRequired/Models/GuidedConversation.swift
@@ -8,7 +8,7 @@
 import Foundation
 import FoundationModels
 
-// MARK: - Model scaffolding students will extend
+// MARK: - Model scaffolding participants will extend
 
 @Generable
 struct GuidedConversation: Codable {
